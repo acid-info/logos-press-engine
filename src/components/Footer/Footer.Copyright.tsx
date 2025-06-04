60e44cafe0f6e6ebaa65944c302ac042d5d06959
JulesFILIOT <dev@julesfiliot.com>
2025-06-04 18:17:35 +0200

    chore(FooterCopyright): update copy

--- src/components/Footer/Footer.Copyright.tsx
+++ src/components/Footer/Footer.Copyright.tsx
@@ -1,16 +1,12 @@
 import { FooterSection } from '@/components/Footer/Footer.Section'
 import { Typography } from '@acid-info/lsd-react'
 import styled from '@emotion/styled'
-import { siteConfigs } from '../../configs/site.configs'
 import { lsdUtils } from '../../utils/lsd.utils'
 
 export const FooterCopyright = () => (
   <OrgInfo>
     <Typography component="div" genericFontFamily="sans-serif" variant="body2">
-      {siteConfigs.title} ©{new Date().getFullYear()}
-    </Typography>
-    <Typography component="div" genericFontFamily="sans-serif" variant="body2">
-      All rights reserved.
+      Logos
     </Typography>
   </OrgInfo>
 )
