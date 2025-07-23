8f645a33bd923b5fbc6401581192731bb063a7fb
JulesFILIOT <dev@julesfiliot.com>
2025-07-23 12:41:57 +0900

    chore: trigger ci

--- src/utils/security.utils.ts
+++ src/utils/security.utils.ts
@@ -13,6 +13,7 @@ export const validateOrigin = (req: NextApiRequest): boolean => {
 
   const originUrl = new URL(origin)
 
+  // todo: remove dev-press.logos.co
   if (
     originUrl.hostname === 'localhost' ||
     originUrl.hostname === '127.0.0.1' ||
