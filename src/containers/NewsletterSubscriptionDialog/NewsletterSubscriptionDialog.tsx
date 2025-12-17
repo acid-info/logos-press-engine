a5bbeeb20de86b4e0ca7c9b3b88b1a99a84acfd3
jinhojang6 <jinho5051@gmail.com>
2025-12-17 20:34:56 +0900

    feat: add umami tracking for newsletter subscriptions and footer interactions

--- src/containers/NewsletterSubscriptionDialog/NewsletterSubscriptionDialog.tsx
+++ src/containers/NewsletterSubscriptionDialog/NewsletterSubscriptionDialog.tsx
@@ -41,8 +41,7 @@ export default function NewsletterSubscriptionDialog({
     const email = e.currentTarget.email.value
     const NEWSLETTER_ID = '6835cf08531d570001068824'
     await subscribe(email, NEWSLETTER_ID)
-    // @ts-ignore
-    window.umami.track('subscribe', { source: 'navbar-newsletter-dialog' })
+    window.umami?.track('subscribe', { source: 'navbar-newsletter-dialog' })
   }
 
   return (
