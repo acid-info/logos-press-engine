56a9ef6785caf51e95eaf6993997f1202293ef92
jinhojang6 <jinho5051@gmail.com>
2026-02-20 02:28:18 +0900

    feat: enhance ArticleFooter and related components with conditional border handling

--- src/components/Article/Footer/Article.RelatedArticles.tsx
+++ src/components/Article/Footer/Article.RelatedArticles.tsx
@@ -2,9 +2,18 @@ import { ArticleReference } from '@/components/ArticleReference'
 import { Collapse } from '@/components/Collapse'
 import { LPE } from '../../../types/lpe.types'
 
-const ArticleRelatedArticles = ({ data }: { data: LPE.Article.Metadata[] }) =>
+const ArticleRelatedArticles = ({
+  data,
+  keepBorderTop = false,
+}: {
+  data: LPE.Article.Metadata[]
+  keepBorderTop?: boolean
+}) =>
   data.length > 0 ? (
-    <Collapse className="related-articles" label="Related Articles">
+    <Collapse
+      {...(keepBorderTop ? { 'data-keep-border-top': 'true' } : {})}
+      label="Related Articles"
+    >
       {data.map((article, idx) => (
         <ArticleReference key={idx} data={article} />
       ))}
